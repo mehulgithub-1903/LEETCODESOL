2a0550bd404e66136c21da4b7f9f12e02d07fc7e
Mehul Porwal <64722605+mehulgithub-1903@users.noreply.github.com>
2022-10-16 00:03:27 +0530

    Time: 6 ms (51.55%), Space: 52.2 MB (20.62%) - LeetHub

---
+++ 0135-candy/0135-candy.java
@@ -0,0 +1,44 @@
+
+class Solution {
+    public int candy(int[] arr) {
+     
+        
+        int n=arr.length;
+        
+        int []left=new int[n];
+        int []right=new int[n];
+        
+        Arrays.fill(left,1);
+        Arrays.fill(right,1);
+
+        int max=0;
+
+        
+        for(int i=1;i<n;i++)
+        {
+          if(arr[i-1]<arr[i])
+          {
+                left[i]=left[i-1]+1;
+        
+          }
+
+        }
+        for(int i=n-2;i>=0;i--)
+        {
+          if(arr[i]>arr[i+1])
+          {
+            right[i]=right[i+1]+1;
+          } 
+
+        }
+        int ans=0;
+            
+        for(int i=0;i<n;i++)
+        {
+          ans+=Math.max(left[i],right[i]);
+        }
+
+        return ans;
+    }
+
+}
