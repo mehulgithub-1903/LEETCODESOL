7b729810889ebf375d3dbe236936a07e6aaababf
Mehul Porwal <64722605+mehulgithub-1903@users.noreply.github.com>
2022-10-22 23:11:56 +0530

    Time: 18 ms (33.72%), Space: 47.1 MB (58.17%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -14,10 +14,16 @@ class Solution {
           return root;
       }
         TreeNode left=lowestCommonAncestor(root.left,p,q);
+//         find anyone or both node in left or return left as null;
+       
+//         find any or both node in right or return right as null;
+        
         TreeNode right=lowestCommonAncestor(root.right,p,q);
         
         if(left==null) return right;
         if(right==null) return left;
+        
+//         if both are not null we means one node is present in right sub and one is present in left subtree
         
         return root;
     }
