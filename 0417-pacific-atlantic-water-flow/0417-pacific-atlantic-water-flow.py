94a0cd36e7ab3e7f7a06b1556b21783ec41f6156
Mehul Porwal <64722605+mehulgithub-1903@users.noreply.github.com>
2022-10-28 14:51:03 +0530

    Time: 675 ms (26.60%), Space: 15.2 MB (81.64%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,6 +1,26 @@
 
 from collections import deque
 class Solution(object):
+    def waterInOcean(self,ocean,heights,rows,cols,visited,q):
+        dr=[0,0,1,-1]
+        dc=[-1,1,0,0]
+        while len(q)!=0:
+            s=len(q)
+            # print("Here")
+            for i in range(s):
+                # print("Hereww")
+                x=q.pop()
+                ocean[x[0]][x[1]]=True
+                for i in range(4):
+                    nr=x[0]+dr[i]
+                    nc=x[1]+dc[i]
+
+                    if nr<rows and nc<cols and nr>=0 and nc>=0 and (not visited[nr][nc]) and heights[nr][nc]>=heights[x[0]][x[1]]:
+                        q.append((nr,nc))
+                        visited[nr][nc]=True;
+
+
+
     def pacificAtlantic(self, heights):
 
         rows=len(heights)
@@ -11,38 +31,20 @@ class Solution(object):
         q=deque()
 
         # q.append((0,0))
+        # water in these cells will surely go in pacific
         for i in range(0,cols):
             q.append((0,i))
             visited[0][i]=True
         for i in range(0,rows):
             if not visited[i][0]:
                 q.append((i,0))
-                visited[i][0]=True        
-        # pacific
-        
-        # print(q)
-        dr=[0,0,1,-1]
-        dc=[-1,1,0,0]
-        while len(q)!=0:
-            s=len(q)
-            # print("Here")
-            for i in range(s):
-                # print("Hereww")
-                x=q.pop()
-                pacific[x[0]][x[1]]=True
-                for i in range(4):
-                    nr=x[0]+dr[i]
-                    nc=x[1]+dc[i]
+                visited[i][0]=True
 
-                    if nr<rows and nc<cols and nr>=0 and nc>=0 and (not visited[nr][nc]) and heights[nr][nc]>=heights[x[0]][x[1]]:
-                        q.append((nr,nc))
-                        visited[nr][nc]=True;
-
-        # atlantic
+        self.waterInOcean(pacific,heights,rows,cols,visited,q)                    
 
         visited=[[False for x in range(cols)] for y in range(rows)]
         q=deque()
-
+        # water in these cells wil surely go in atlantic
         for i in range(cols):
             q.append((rows-1,i))
             visited[rows-1][i]=True
@@ -51,22 +53,10 @@ class Solution(object):
                 q.append((i,cols-1))
                 visited[i][cols-1]=True        
 
-        dr=[0,0,1,-1]
-        dc=[-1,1,0,0]
-        while len(q)!=0:
-            s=len(q)
-            # print("Here")
-            for i in range(s):
-                x=q.pop()
-                atlantic[x[0]][x[1]]=True
-                for i in range(4):
-                    nr=x[0]+dr[i]
-                    nc=x[1]+dc[i]
+        self.waterInOcean(atlantic,heights,rows,cols,visited,q) 
 
-                    if nr<rows and nc<cols and nr>=0 and nc>=0 and (not visited[nr][nc]) and heights[nr][nc]>=heights[x[0]][x[1]]:
-                        q.append((nr,nc))
-                        visited[nr][nc]=True;
-
+        # ans will be the intersection of cells who are true in pacific matrix (meaning there water will flow in pacific)
+        # and are also true in atlantic matrix (meaning there will also flow in atlantic)
 
         ans=[]
         for k in range(rows):
@@ -75,8 +65,6 @@ class Solution(object):
                     ans.append((k,e))
 
         return ans
-
-
 
 
 
