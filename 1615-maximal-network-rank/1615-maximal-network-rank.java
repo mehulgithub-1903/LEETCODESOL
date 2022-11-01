e943364b070c26e798b98b22492e9d508960d139
Mehul Porwal <64722605+mehulgithub-1903@users.noreply.github.com>
2022-11-02 01:02:03 +0530

    Time: 19 ms (51.86%), Space: 54.6 MB (38.48%) - LeetHub

--- 1615-maximal-network-rank/1615-maximal-network-rank.java
+++ 1615-maximal-network-rank/1615-maximal-network-rank.java
@@ -3,11 +3,11 @@ import java.util.ArrayList;
 class Solution {
     public int maximalNetworkRank(int n, int[][] roads) {
         
-        ArrayList<Set<Integer>> adj = new ArrayList<Set<Integer>>();
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>();
 
         for(int i=0;i<n;i++)
         {
-            adj.add(new HashSet<Integer>());
+            adj.add(new ArrayList<Integer>());
         }
         for(int e[]:roads)
         {
@@ -16,25 +16,106 @@ class Solution {
         }
         int result=0;
 
-        for(int i=0;i<n;i++)
+        int degree[]=new int[n];
+
+        for(int road[]:roads)
         {
-            for(int j=0;j<n;j++)
+            int f=road[0];
+            int t=road[1];
+
+            degree[f]++;
+            degree[t]++;
+        }
+        
+        // for(int e:degree) System.out.println(e);
+        int md=-1;
+        int smd=-1;
+        for(int e:degree)
+        {
+            if(e>md)
             {
-                if(i!=j)
+                smd=md;
+                md=e;
+                
+            }
+            else if(e>smd)
+            {
+                smd=e;
+            }
+        }
+        
+        // System.out.println(md);
+        // System.out.println(smd);
+        
+
+        int cmd=0;
+        int csmd=0;
+        for(int e:degree)
+        {
+            if(e==md)
+            {
+              cmd++;  
+            }
+            if(e==smd)
+            {
+                csmd++;
+            }
+        }
+        if(cmd==1)
+        {
+            // try to search for a node with degree = smd and is not connected to md node 
+            int cc=0;
+            for(int road[]:roads)
+            {
+                int f=road[0];
+                int t=road[1];
+
+               
+                if((degree[f]==smd && degree[t]==md) || (degree[t]==smd && degree[f]==md))
                 {
-                    if(adj.get(i).contains(j))
+                    if(adj.get(f).contains(t))
                     {
-                     result =Math.max(result,adj.get(i).size()+adj.get(j).size()-1);   
-                    }
-                    else
-                    {
-                        result =Math.max(result,adj.get(i).size()+adj.get(j).size());
+                       cc++; 
                     }
                 }
             }
+                if(csmd>cc)
+                {
+                    return md+smd;
+                }
+                else
+                {
+                    return md+smd-1;
+                }
+            
         }
+        else
+        {
+            int cc=0;
+            for(int road[]:roads)
+            {
+                int f=road[0];
+                int t=road[1];
 
-        return result;
-
+                
+                if(degree[f]==md && degree[t]==md)
+                {
+                    if(adj.get(f).contains(t))
+                    {
+                       cc++; 
+                    }
+                }
+            }
+                if((cmd*(cmd-1)/2)>cc)
+                {
+                    return md+md;
+                }
+                else
+                {
+                    return md+md-1;
+                }
+            
+        }
+        
     }
 }
